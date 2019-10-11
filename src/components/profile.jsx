0c8371dcc4c88f5ffe269dede93c80b0076fd90d
monsoonjunkie <anthonyle925@gmail.com>
2019-10-11 14:46:11 -0700

    added architecture of app

---
+++ src/components/profile.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import 'profile_styling.css';
+
+class Profile extends React.Component {
+
+    render() {
+        return(
+            <div className="">
+                <div className="">
+                    <div className="">
+                        <div className="">Matches</div>
+                        <div className="">0</div>
+                        <div className="">PLAYER 2</div>
+                    </div>
+                </div>
+            </div>
+        )
+    }
+}
+
+export default Profile;
+
+
+
+
