04ba23cc160ca1dc22a0fcf0bf44938c40af2beb
monsoonjunkie <anthonyle925@gmail.com>
2019-10-16 21:04:40 -0700

    added styling to help component

--- src/components/help.jsx
+++ src/components/help.jsx
@@ -1,21 +1,26 @@
 import React from 'react';
 import './help_styling.css';
+import {how_to} from '../assets/';
 
 class Help extends React.Component {
     render() {
         return(
-            <div>
-                <div>How to Play</div>
-                <div>
-                    <img src="../assets/how_to.gif" alt=""/>
+            <div className="help_container">
+                <div className="how_to_heading">How to Play</div>
+                <div className="help_bg">
+                    <img className="help_tip"src={how_to} alt=""/>
                 </div>
 
-                <div class="">Welcome to the Persona 5 Memory Match Game! </div>
-            <div class="">
-                <div class="col"> Persona 5 Memory Match is a 2 player game where each player takes turns flipping cards to match a pair.</div>
-                <div class="col">Pick which player you want to be and just start flipping cards. Once all the cards have been matched, the player with the most matched pairs wins!</div>
-                <div class="col">I hope you enjoy playing this as much as I enjoy making it. Thank you!</div>
+                <div className="how_to_heading help_heading2">Welcome to the Persona 5 Memory Match Game! </div>
+            <div className="help_heading3">
+               
+                    <div className="">Persona 5 Memory Match is a 2 player game where each player takes turns flipping cards to match a pair.</div>
+                    <div className="">Pick which player you want to be and just start flipping cards. Once all the cards have been matched, the player with the most matched pairs wins!</div>
+                    <div className="">I hope you enjoy playing this as much as I enjoy making it. Thank you!</div>
+            
+                
             </div>
+                <div className="help_close">Close</div>
             </div>
         )
     }
