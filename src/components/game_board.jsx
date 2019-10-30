e0059c81f2e38e7a9961f9df20ad87063fc543e9
monsoonjunkie <anthonyle925@gmail.com>
2019-10-29 21:13:15 -0700

    completed card component to flip when clicked

--- src/components/game_board.jsx
+++ src/components/game_board.jsx
@@ -3,38 +3,46 @@ import { connect } from 'react-redux';
 import Cardrow from './card_row.jsx';
 import {one,two,three,four,five,six,seven,eight,nine} from '../assets/';
 import './game_board_styling.css';
-import {loadDeck} from '../../src/store/gameboard/gameboard_actions';
+import {loadDeck, cardToggle} from '../../src/store/gameboard/gameboard_actions';
 
 
 
 class Gameboard_Base extends React.Component {
     constructor(props){
         super(props);
-        console.log('unmounted');
+        this.cardClick = this.cardClick.bind(this);
     }
 
     componentDidMount() {
-      console.log('mounted');
       this.props.loadDeck(this.randomList());
     }
-
-
+    cardClick(id){
+      this.props.cardClick(id);
+    }
+    createCardObj(path,index){
+      return {
+        id: index,
+        path: path,
+        revealed: false
+      }
+    }
     randomList(){
         const cardArray = [one,two,three,four,five,six,seven,eight,nine];
-        let doubleArray = cardArray.concat(cardArray);
-        let currentIndex = doubleArray.length, temporaryValue, randomIndex;
+        const bigArray = cardArray.concat(cardArray);
+        const cardObjArr = bigArray.map(this.createCardObj);
+        let currentIndex = cardObjArr.length, temporaryValue, randomIndex;
         
         while (0 !== currentIndex) {
         
             randomIndex = Math.floor(Math.random() * currentIndex);
             currentIndex -= 1;
         
-            temporaryValue = doubleArray[currentIndex];
-            doubleArray[currentIndex] = doubleArray[randomIndex];
-            doubleArray[randomIndex] = temporaryValue;
+            temporaryValue = cardObjArr[currentIndex];
+            cardObjArr[currentIndex] = cardObjArr[randomIndex];
+            cardObjArr[randomIndex] = temporaryValue;
         }
         
-        return doubleArray;
+        return cardObjArr;
     }
     spliceList(array){
         let newArray = array.splice(0,6);
@@ -42,51 +50,30 @@ class Gameboard_Base extends React.Component {
     }
     newDeck() {
       let storeDeck = this.props.gameboard.deck;
-      if(storeDeck.length === 0){
+      if(storeDeck.length > 0){
 
       }
 
     }
 
     render(){
-        console.log('props', this.props);
-        console.log('deck',this.props.gameboard.deck)
         
-
-        let shuffleArray = this.randomList();
-        let cardList1 = this.spliceList(shuffleArray);
-        let cardList2 = this.spliceList(shuffleArray);
-        let cardList3 = this.spliceList(shuffleArray);
+        let deck = [...this.props.gameboard.deck];
+        let cardList1 = this.spliceList(deck);
+        let cardList2 = this.spliceList(deck);
+        let cardList3 = this.spliceList(deck);
 
         return(
             <div className = "card_space">
-                <Cardrow cardArray={cardList1}/>
-                <Cardrow cardArray={cardList2}/>
-                <Cardrow cardArray={cardList3}/>
+                <Cardrow cardClicked={this.cardClick} cardArray={cardList1}/>
+                <Cardrow cardClicked={this.cardClick} cardArray={cardList2}/>
+                <Cardrow cardClicked={this.cardClick} cardArray={cardList3}/>
             </div>
         )
     }
 }
-function randomList(){
-  const cardArray = [one,two,three,four,five,six,seven,eight,nine];
-  let doubleArray = cardArray.concat(cardArray);
-  let currentIndex = doubleArray.length, temporaryValue, randomIndex;
-  
-  while (0 !== currentIndex) {
-  
-      randomIndex = Math.floor(Math.random() * currentIndex);
-      currentIndex -= 1;
-  
-      temporaryValue = doubleArray[currentIndex];
-      doubleArray[currentIndex] = doubleArray[randomIndex];
-      doubleArray[randomIndex] = temporaryValue;
-  }
-  
-  return doubleArray;
-}
 
 const mapStateToProps = state => {
-    console.log('state', state)
     const gameboard = state.gameboard
     return {
         gameboard
@@ -98,8 +85,8 @@ const mapStateToProps = state => {
       loadDeck: deck => {
         dispatch(loadDeck(deck));
       },
-      updateDeck: id => {
-          console.log('updated', id);
+      cardClick: index => {
+        dispatch(cardToggle(index));
       }
     }
   }
--- src/store/gameboard/gameboard_actions.js
+++ src/store/gameboard/gameboard_actions.js
@@ -2,3 +2,9 @@ export const loadDeck = deck => ({
     type: '[GAMEBOARD] LOAD_DECK', 
     payload: deck
   })
+
+export const cardToggle = (id) => ({
+  type: '[GAMEBOARD] TOGGLE_CARD', 
+  payload: id
+})
+
